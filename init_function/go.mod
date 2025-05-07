0d70fc1c2339b33e9e96b9b5b872b52e089c9598
jihanurrahman33 <jihanurrahman33@gmail.com>
2025-05-07 18:16:44 +0600

    init function practice

---
+++ init_function/go.mod
@@ -0,0 +1,3 @@
+module example.com/init_function
+
+go 1.24.1
---
+++ init_function/init_function.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+func main(){
+	fmt.Println("Main function called")
+
+}
+func init(){
+	fmt.Println("init called first")
+}
